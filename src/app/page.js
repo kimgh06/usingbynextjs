37439c4efe356f450a94ca5a74f99f178d38eb35
kkh061101 <kkh061101@naver.com>
2023-05-15 16:09:08 +0900

    'asdf'

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+"use client";
+import { useState } from "react"
+
 export default function Home() {
+  const [cnt, setCnt] = useState(0);
   return (
     <div>
-      hi
+      hi{cnt}
+      <button onClick={e => setCnt(cnt + 1)}>+</button>
     </div>
   )
 }
