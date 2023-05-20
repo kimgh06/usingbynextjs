181d76e77561c80325a8056b567c75a40af03469
kkh061101 <kkh061101@naver.com>
2023-05-20 21:04:04 +0900

    style jsx 못쓰겠다 계속 import trace error 나서

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,13 @@ import Header from "@/components/Header";
 import axios from "axios";
 import { useEffect, useState } from "react";
 const apikey = "5035c021fc6a2236a7950c34e421ac51";
-const url = "https://api.themoviedb.org/3/";
+const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/w500";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
   const fetching = async e => {
-    await axios.get(`${url}movie/popular?api_key=${apikey}`).then(e => {
+    await axios.get(`${url}/movie/popular?api_key=${apikey}`).then(e => {
       console.log(e.data.results);
       setMovies(e.data.results);
     });
@@ -18,10 +19,11 @@ export default function Home() {
     fetching();
   }, []);
   return (
-    <div>
+    <div className="container">
       <Header title={"index"} />
       {movies?.length > 0 ? movies.map(i => {
-        return <div key={i?.id}>
+        return <div className="movie" key={i?.id}>
+          <img src={`${imgUrl}/${i?.poster_path}`} />
           <h4>{i?.original_title}</h4>
         </div>
       }) : <h4>Loading</h4>}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 "use client";
+import "../app/styles.scss";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -19,32 +20,6 @@ const Navbar = () => {
         </span>
       </Link>
     </div>
-    <style jsx>{`nav {
-          display: flex;
-          gap: 10px;
-          flex-direction: column;
-          align-items: center;
-          padding-top: 20px;
-          padding-bottom: 10px;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-        }
-        img {
-          max-width: 100px;
-          margin-bottom: 5px;
-        }
-        nav a {
-          font-weight: 600;
-          font-size: 18px;
-        }
-        .active {
-          color: tomato;
-        }
-        nav div {
-          display: flex;
-          gap: 10px;
-        }
-      `}</style>
   </nav>;
 }
 
