f3de4afa9264151b97a8b8b190fb195521b053e6
kkh061101 <kkh061101@naver.com>
2023-05-22 08:35:00 +0900

    use getData()

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,28 @@
 "use client";
 
 import Header from "@/components/Header";
-import axios from "axios";
+import { use } from "react";
 import { useEffect, useState } from "react";
 const apikey = "5035c021fc6a2236a7950c34e421ac51";
 const url = "https://api.themoviedb.org/3";
 const imgUrl = "https://image.tmdb.org/t/p/w500";
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
-  const fetching = async e => {
-    await axios.get(`/api/movies`).then(e => {
-      console.log(e.data.results);
-      setMovies(e.data.results);
-    });
-  }
-  useEffect(e => {
-    fetching();
-  }, []);
-  return (
+  const movies = use(getData()).results;
+  return <>
     <div className="container">
       <Header title={"index"} />
-      {movies?.length > 0 ? movies.map(i => {
+      {movies?.map(i => {
         return <div className="movie" key={i?.id}>
           <img src={`${imgUrl}/${i?.poster_path}`} />
           <h4>{i?.original_title}</h4>
         </div>
-      }) : <h4>Loading</h4>}
+      })}
     </div>
-  )
+  </>;
+}
+
+export async function getData() {
+  const movies = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return movies;
 }
