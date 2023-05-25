65d2f27e6e442a8e93b37713f60380a958bc0028
kkh061101 <kkh061101@naver.com>
2023-05-25 15:40:00 +0900

    성공적으로 데이터 불러오기

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const apikey = process.env;
+const apikey = "5035c021fc6a2236a7950c34e421ac51";
 const url = "https://api.themoviedb.org/3";
 const nextConfig = {
   async redirects() {
@@ -18,7 +18,7 @@ const nextConfig = {
       destination: `${url}/movie/popular?api_key=${apikey}`
     }, {
       source: '/api/movies/:id',
-      destination: `${url}/movie/:id`
+      destination: `${url}/movie/:id?api_key=${apikey}`
     }];
   }
 }
--- src/app/movies/[...params]/page.js
+++ src/app/movies/[...params]/page.js
@@ -5,19 +5,9 @@ import { use, useEffect, useState } from 'react';
 export default function home({ params }) {
   const [data, setData] = useState();
   const asdf = async e => {
-    const url = 'https://api.themoviedb.org/3/movie/385687?language=en-US';
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer 5035c021fc6a2236a7950c34e421ac51'
-      }
-    };
-
-    fetch(url, options)
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.error('error:' + err));
+    await axios.get(`http://localhost:3000/api/movies/${params.params[0]}`).then(e => {
+      console.log(e.data);
+    })
   }
   useEffect(e => {
     asdf();
